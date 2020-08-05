2cc6974a6dbdbb0a968f26d228900c4accf765fb
maymaymay722 <meiyingying0722@163.com>
2020-08-06 06:47:12 +0800

    经典算法排序

---
+++ 排序/src/sort/InsertSort.java
@@ -0,0 +1,24 @@
+package sort;
+
+/*
+插入排序
+ */
+public class InsertSort {
+    public static int[] insertSort(int[] array){
+        if(array.length==0){
+            return array;
+        }
+
+        int current;
+        for (int i = 0; i < array.length-1; i++) {
+            current=array[i+1];
+            int preIndex=i;
+            while(preIndex>=0 && current<array[preIndex]){
+                array[preIndex+1]=array[preIndex];
+                preIndex--;
+            }
+            array[preIndex+1]=current;
+        }
+        return array;
+    }
+}
